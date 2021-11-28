8d14f2f5bb367050c5986b26174dd769739de35b
Carl Börstell <calleborstell@gmail.com>
2021-11-28 16:19:48 +0100

    Add files via upload

--- split_pano.R
+++ split_pano.R
@@ -1,8 +1,8 @@
 split_pano <- function(path, ratio="square") {
   img <- magick::image_read(path)
   vls <- magick::image_attributes(img)
-  height <- vls$value[7]
-  width <- vls$value[8]
+  height <- vls$value[31]
+  width <- vls$value[30]
   ratio_factor <- 1
   if (ratio=="wide") {
     ratio_factor <- 1.91
@@ -16,6 +16,7 @@ split_pano <- function(path, ratio="square") {
   path_name <- gsub(file_name, "", path)
   extension <- paste0(".",tools::file_ext(file_name))
   fname <- gsub(extension, "", file_name)
+  print(num)
   for (n in 1:num) {
     im <- magick::image_crop(img, geometry = paste0(crop_width,"x",height,"+",(n-1)*crop_width,"+",0))
     magick::image_write(im, paste0(path_name,fname,"_",n,extension))
